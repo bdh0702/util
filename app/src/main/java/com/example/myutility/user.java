923c44053a2050bd6e410b9f958948865143add3
bdh0702 <bdh0702@naver.com>
2019-08-12 10:42:19 +0900

    1 commit

---
+++ app/src/main/java/com/example/myutility/user.java
@@ -0,0 +1,18 @@
+package com.example.myutility;
+
+public class user {
+    public String nickname;
+    public String id;
+    public String email;
+    public String password;
+
+    public user(){
+
+    }
+    public user(String nickname,String id,String email,String password){
+        this.nickname = nickname;
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
+}
